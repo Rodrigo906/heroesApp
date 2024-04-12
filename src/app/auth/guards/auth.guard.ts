26be3e194a32b05edbe6b0ab9a25cff48d4ce5e7
Rodrigo Calizaya <rcalizayadev@gmail.com>
2024-04-12 13:03:14 -0300

    Finalizacion de modulo 16

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,71 @@
+import { inject } from '@angular/core';
+
+import { Observable, tap } from 'rxjs';
+
+import {
+
+  ActivatedRouteSnapshot,
+  CanActivateFn,
+  CanMatchFn,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+
+} from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+
+const checkAuthStatus = (): boolean | Observable<boolean> => {
+
+  //se inyectan el AuthService y el Router
+
+  const authService: AuthService = inject(AuthService);
+
+  const router: Router = inject(Router);
+
+  return authService.checkAuthentication().pipe(
+
+    tap((isAuthenticated) => {
+
+      if (!isAuthenticated) {
+
+        router.navigate(['/auth/login']);
+
+      }
+
+    })
+
+  );
+
+};
+
+export const canActivateAuthGuard: CanActivateFn = (
+
+  route: ActivatedRouteSnapshot,
+
+  state: RouterStateSnapshot
+
+) => {
+
+  console.log('CanActivate');
+  console.log({ route, state });
+
+  return checkAuthStatus();
+
+};
+
+export const canMatchAuthGuard: CanMatchFn = (
+
+  route: Route,
+  segments: UrlSegment[]
+
+) => {
+
+  console.log('CanMatch');
+  console.log({ route, segments });
+
+  return checkAuthStatus();
+};
+
+
